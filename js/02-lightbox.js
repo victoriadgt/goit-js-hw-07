d36c02c2f94e946742b8977fbbb8d9c82a4cf7b6
victoriadgt <victoria.daguta@gmail.com>
2024-11-07 12:02:24 +0200

    tema7 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Crearea și randarea marcajului
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+    </a>
+  </li>
+`).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Inițializarea librăriei SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
